27b0cdb51a0295038a61be17d9c2b86e5f48fa82
Adarsh Namdev <adarsh.namdev89@gmail.com>
2021-05-07 20:07:52 +0530

    Create Regex1.py

---
+++ Regex1.py
@@ -0,0 +1,28 @@
+
+"""
+Find all the IP addresses from the txt file and store them in table
+"""
+
+from prettytable import PrettyTable
+import re
+
+with open(r"D:\PythonProgram\FileHandling\mbox-short.txt", 'r') as handle:
+    IPAddress = []
+    ipTable = PrettyTable()
+    line_num = 1
+    ipTable.field_names= ["#", "Line from txt file", "IP Address"]
+
+    for line in handle:
+        line = line.strip()
+        #IPAddress = IPAddress + re.match("\b[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\b", line)
+        IPAddress = re.findall("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", line)
+
+        if len(IPAddress) < 1:
+            continue
+
+        else:
+            ipTable.add_row([line_num, line, " ".join(IPAddress)])
+            line_num += 1
+
+
+print(ipTable)
