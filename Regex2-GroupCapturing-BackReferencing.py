e4d0dfefbd11db4afb9e4e4b7e73bd776a32a198
Adarsh Namdev <adarsh.namdev89@gmail.com>
2021-05-12 20:00:41 +0530

    Update Regex2-GroupCapturing-BackReferencing.py

--- Regex2-GroupCapturing-BackReferencing.py
+++ Regex2-GroupCapturing-BackReferencing.py
@@ -8,7 +8,7 @@ with open(r"C:\Users\adars\Desktop\random.txt", "r") as handle1:
     content1 = handle1.read()
     print("Actual Content:\n", content1,"\n####################################################\n")
 
-    print("obtaining the phone numbers in this format 123-xxx-1234")
+    print("obtaining the phone numbers in the format: 123-xxx-1234")
     print("---------------------------------------------------------")
     redact_ph = re.sub(r"(\d{3})-\d{3}-(\d{4})", r"\1-xxx-\2", content1)
     print(redact_ph)
@@ -17,3 +17,52 @@ with open(r"C:\Users\adars\Desktop\random.txt", "r") as handle1:
     print("-------------------------------------------------------")
     mm_dd_yyyy = re.sub(r"(\d{1,2})\s?([a-zA-Z]{3})\s?(\d{4})", r"\2 \1, \3", content1)
     print(mm_dd_yyyy)
+
+
+"""
+Output:
+``````
+Actual Content:
+ 19 Jan 1965
+Judy Foster
+912-576-7052
+
+02 Aug 1984
+John Nash
+717-852-7391
+
+30 Jun 1989
+Baba Sikandar
+750-715-7178
+
+####################################################
+
+obtaining the phone numbers in this format 123-xxx-1234
+---------------------------------------------------------
+19 Jan 1965
+Judy Foster
+912-xxx-7052
+
+02 Aug 1984
+John Nash
+717-xxx-7391
+
+30 Jun 1989
+Baba Sikandar
+750-xxx-7178
+
+obtaining date format from mm dd yyyy to mm dd, yyyy
+-------------------------------------------------------
+Jan 19, 1965
+Judy Foster
+912-576-7052
+
+Aug 02, 1984
+John Nash
+717-852-7391
+
+Jun 30, 1989
+Baba Sikandar
+750-715-7178
+
+"""
