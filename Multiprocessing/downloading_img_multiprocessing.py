e2227c3da6e95623ecdb46b32f90782f0b07ab6a
Adarsh Namdev <adarsh.namdev89@gmail.com>
2023-09-14 14:36:57 +0530

    Update downloading_img_multiprocessing.py
    code to download images from website https://picsum.photos/ concurrently using the multiprocessing module.

--- Multiprocessing/downloading_img_multiprocessing.py
+++ Multiprocessing/downloading_img_multiprocessing.py
@@ -3,17 +3,27 @@ import requests
 import multiprocessing
 import os
 
-def downliad_img(url, filename, target_dir):
+def download_img(imgurl, filename, target_dir):
+    repsonse_obj = requests.get(url=imgurl)
+    with open(f"{target_dir}/{filename}.jpg", 'wb') as fh:
+        fh.write(repsonse_obj.content)
 
+    print(f"image - {filename}.jpg downloaded")
 
 
 if __name__ == '__main__':
-    dir = "D:\PWSkills_DS_Masters\DownloadImageUsingMultiprocessing"
+    dir = "D:\PWSkills_DS_Masters\DownloadImageUsingMultiproc"
+    img_url = "https://picsum.photos/2000/3000"
     if not os.path.exists(dir):
         os.mkdir(dir)
-
 
     else:
         pass
     
-"D:\PWSkills_DS_Masters\DownloadImageUsingMultiprocessing"
+    list_of_processes = []
+    for i in range(0,5):
+        process = multiprocessing.Process(target = download_img, args = [img_url, f"img_{i}", dir])
+        list_of_processes.append(process)
+    
+    for proc in list_of_processes:
+        proc.start()
