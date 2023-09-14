0e7cb936904fa62f5d818f56d10668d9b534b312
Adarsh Namdev <adarsh.namdev89@gmail.com>
2023-09-14 16:31:33 +0530

    first commit

---
+++ GCP/CreateTableFromGCSFile.py
@@ -0,0 +1,9 @@
+
+"""
+Author       : Adarsh Namdev
+Activity     : GCP POC
+GCP Service  : BigQuery
+Task(s)      :
+              - create table out of GCS file(can be bq and python library)
+                format : csv, parquet, json
+"""
